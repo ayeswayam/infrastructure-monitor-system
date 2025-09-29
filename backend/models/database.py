f4b49ad31ea2b6080c907e750f3d5480e2cdcdcb
Swayam Singh <114747061+ayeswayam@users.noreply.github.com>
2025-09-29 18:12:43 +0530

    Update database.py

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,7 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 def init_db(app):
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        try:
+            db.create_all()
+        except Exception as e:
+            print(f"Database error: {e}")
